4e478edccf7ef1ad68034efbb93a800f2ef3c3e5
elenashestakova <blacksilence@email.com>
2023-04-19 14:20:17 +0300

    task (2.3) done

--- lvl_2/task_2.3.py
+++ lvl_2/task_2.3.py
@@ -8,9 +8,39 @@
 # Использовать условный оператор if-elif-else нельзя!
 
 def switch_it_up(number):
-    digits_words = ["Zero", "One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
-    return digits_words[number] if 0 <= number <= 9 else None
+    match number:
+        case 0:
+            return 'Zero'
+        case 1:
+            return 'One'
+        case 2:
+            return'Two'
+        case 3:
+            return'Three'
+        case 4:
+            return 'Four'
+        case 5:
+            return 'Five'
+        case 6:
+            return 'Six'
+        case 7:
+            return 'Seven'
+        case 8:
+            return 'Eight'
+        case 9:
+            return 'Nine'
+        case _:
+            return 'None'
+
+
+    #  digits_words = ["Zero", "One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
+    #  return digits_words[number] if 0 <= number <= 9 else None
 
 
 num = int(input('Введите цифру от 0 до 9: '))
 print(switch_it_up(num))
+
+
+
+
+
